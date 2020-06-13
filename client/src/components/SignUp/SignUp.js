5999f9a5c77cd433b1a772f7138dae84f3155289
srijita kiki <kikisrijita@gmail.com>
2020-06-13 20:38:31 +0530

    basic signup state added

--- client/src/components/SignUp/SignUp.js
+++ client/src/components/SignUp/SignUp.js
@@ -1,20 +1,37 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { SignUpAnimation } from './../../utils/SignUp';
-import './SignUp.css';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { SignUpAnimation } from "./../../utils/SignUp";
+import "./SignUp.css";
+import axios from "axios";
 
 const SignUp = () => {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
   });
-  const [responseError, setResponseError] = useState('');
+  const [responseError, setResponseError] = useState("");
 
   useEffect(() => {
     SignUpAnimation();
   }, []);
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      if (formData.password === formData.confirmPassword) {
+        let response = await axios.post("/api/v1/auth/register", formData);
+        console.log(response.data);
+      } else {
+        setResponseError("Incorrect match, please check your password");
+        console.log(responseError);
+      }
+    } catch (error) {
+      setResponseError(error.response.data.error);
+      console.log(responseError);
+    }
+  };
 
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -24,11 +41,11 @@ const SignUp = () => {
     <>
       <div className="container-auth">
         <div className="img">
-          <img src={require('../../img/signup.svg')} alt="signup" />
+          <img src={require("../../img/signup.svg")} alt="signup" />
         </div>
         <div className="login-content">
-          <form onSubmit={onSubmit}>
-            <img src={require('../../img/avatar.svg')} alt="signin" />
+          <form onSubmit={(e) => onSubmit(e)}>
+            <img src={require("../../img/avatar.svg")} alt="signin" />
             <h2 className="title">Lets Connect</h2>
             <div className="input-div one">
               <div className="i">
@@ -98,6 +115,9 @@ const SignUp = () => {
               </div>
             </div>
             <Link to="/signin">Signin here</Link>
+            {responseError ? (
+              <span className="reserror">{responseError}</span>
+            ) : null}
             <input type="submit" className="btn" value="Signup" />
           </form>
         </div>
