4b88bd5912f0cdb51d73efb24e1c648a93b436b3
srijita kiki <kikisrijita@gmail.com>
2020-06-22 16:34:42 +0530

    added modal form functionality

--- client/src/components/AccountForm/AccountForm.js
+++ client/src/components/AccountForm/AccountForm.js
@@ -1,18 +1,48 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import 'react-responsive-modal/styles.css';
-import { Modal } from 'react-responsive-modal';
-import { connect } from 'react-redux';
-import './AccountForm.css';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "react-responsive-modal/styles.css";
+import { Modal } from "react-responsive-modal";
+import { connect } from "react-redux";
+import "./AccountForm.css";
 
 const AccountForm = ({ user }) => {
-  const [modalState, setModalState] = useState(false);
+  const [modalNameState, setModalNameState] = useState(false);
+  const [modalPassState, setModalPassState] = useState(false);
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+    newpassword: "",
+    oldpassword: "",
+  });
+
+  const onHandleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const onHandleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({
+      name: "",
+      password: "",
+      newpassword: "",
+      oldpassword: "",
+    });
+    onCloseModal();
+  };
 
   const onOpenModal = () => {
-    setModalState(true);
+    setModalNameState(true);
   };
+
+  const onOpenPasswordModal = () => {
+    setModalPassState(true);
+  };
+
   const onCloseModal = () => {
-    setModalState(false);
+    setModalNameState(false);
+  };
+  const onClosePassModal = () => {
+    setModalPassState(false);
   };
 
   return (
@@ -32,8 +62,43 @@ const AccountForm = ({ user }) => {
             <Link to="#" className="btn-edit" onClick={onOpenModal}>
               <i class="fas fa-pencil-alt"></i> Edit
             </Link>
-            <Modal open={modalState} onClose={onCloseModal} center>
-              <h4>Simple centered modal</h4>
+            <Modal open={modalNameState} onClose={onCloseModal} center>
+              <form
+                onSubmit={(e) => onHandleSubmit(e)}
+                style={{ padding: "2rem" }}
+              >
+                <label
+                  style={{
+                    display: "block",
+                    width: "100%",
+                    marginBottom: "15px",
+                    fontSize: "17px",
+                  }}
+                  for="fname"
+                >
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  placeholder="Your name.."
+                  style={{
+                    display: "block",
+                    padding: "0.8rem",
+                    marginBottom: "15px",
+                    width: "100%",
+                  }}
+                  onChange={(e) => onHandleChange(e)}
+                  value={formData.name}
+                />
+
+                <input
+                  type="submit"
+                  value="Submit"
+                  style={{ display: "block", width: "100%", cursor: "pointer" }}
+                />
+              </form>
             </Modal>
           </div>
         </div>
@@ -54,9 +119,87 @@ const AccountForm = ({ user }) => {
             <div className="name">Change password for your account here</div>
           </div>
           <div>
-            <Link to="#" className="btn-edit" onClick={onOpenModal}>
+            <Link to="#" className="btn-edit" onClick={onOpenPasswordModal}>
               <i class="fas fa-pencil-alt"></i> Edit
             </Link>
+            <Modal open={modalPassState} onClose={onClosePassModal} center>
+              <form style={{ padding: "2rem" }}>
+                <label
+                  style={{
+                    display: "block",
+                    marginBottom: "15px",
+                    fontSize: "17px",
+                  }}
+                  for="fname"
+                >
+                  Old Password
+                </label>
+                <input
+                  type="password"
+                  id="password-1"
+                  name="oldpassword"
+                  placeholder="Enter Old Password"
+                  style={{
+                    display: "block",
+                    padding: "0.8rem",
+                    marginBottom: "15px",
+                    width: "100%",
+                  }}
+                />
+
+                <label
+                  style={{
+                    display: "block",
+                    marginBottom: "15px",
+                    fontSize: "17px",
+                  }}
+                  for="fname"
+                >
+                  New Password
+                </label>
+                <input
+                  type="password"
+                  id="password-2"
+                  name="newpassword"
+                  placeholder="Enter New Password"
+                  style={{
+                    display: "block",
+                    padding: "0.8rem",
+                    marginBottom: "15px",
+                    width: "100%",
+                  }}
+                />
+
+                <label
+                  style={{
+                    display: "block",
+                    marginBottom: "15px",
+                    fontSize: "17px",
+                  }}
+                  for="fname"
+                >
+                  Confirm Password
+                </label>
+                <input
+                  type="password"
+                  id="password-3"
+                  name="confirmpassword"
+                  placeholder="Confirm Password."
+                  style={{
+                    display: "block",
+                    padding: "0.8rem",
+                    marginBottom: "15px",
+                    width: "100%",
+                  }}
+                />
+
+                <input
+                  type="submit"
+                  value="Submit"
+                  style={{ display: "block", width: "100%", cursor: "pointer" }}
+                />
+              </form>
+            </Modal>
           </div>
         </div>
       </div>
