44131d922e0c0a65213911debb6ed75497fb617e
Ayan Banerjee <ayanbbb354@gmail.com>
2020-07-14 16:51:23 +0530

    minor chnages

--- client/src/components/DiscussionItem/DiscussionItem.js
+++ client/src/components/DiscussionItem/DiscussionItem.js
@@ -1,6 +1,6 @@
-import React from "react";
-import "./DiscussionItem.css";
-import moment from "moment";
+import React from 'react';
+import './DiscussionItem.css';
+import moment from 'moment';
 
 const DiscussionItem = (props) => {
   console.log(props);
@@ -9,8 +9,7 @@ const DiscussionItem = (props) => {
       <React.Fragment>
         <div
           className="discussion-card"
-          onClick={props.goToQsPage ? props.goToQsPage : null}
-        >
+          onClick={props.goToQsPage ? props.goToQsPage : null}>
           <div className="header">
             <div className="user-logo">
               <span>{props.el.user.name[0]}</span>
@@ -18,7 +17,7 @@ const DiscussionItem = (props) => {
             <div className="main-head">
               <h2>{props.el.user.name}</h2>
               <p>Stripe with react and Node crash course</p>
-              <span>{moment(props.el.date).format("MMM Do YY")}</span>
+              <span>{moment(props.el.date).format('MMM Do YY')}</span>
             </div>
           </div>
           <div className="description">
